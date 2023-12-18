9135b527f5be450874b352cd36b631d36654c9b9
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 17:29:11 +0300

    feat: add solution

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int count = cathetusLength;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (j >= count) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            count--;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        if (numberToPrint!=0){
+            int mul = numberToPrint;
+            for (int i = 1; i <= power; i++) {
+                numberToPrint *= mul;
+            }
+            System.out.println(numberToPrint / mul);
+        } else {
+            System.out.println(0);
+        }
     }
 
     public static void main(String[] args) {
